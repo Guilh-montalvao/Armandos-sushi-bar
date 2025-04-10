30b16c15f4b90010ce5ff75f35a28086d5664150
Guilherme Augusto montalvão <guilherme06075828125@gmail.com>
2025-04-10 08:49:07 -0300

    style: add color variables

---
+++ src/Style/colors.ts
@@ -0,0 +1,20 @@
+export const colors = {
+    black: {
+      Primary: "#000000",
+      textPrimary: "#323232"
+    },
+  
+    white: {
+      Background: "#FFFFFF",
+    },
+    
+    gray: {
+      stroke: "#EBEBEB",
+      input: "#E4E4E7",
+      textSecundary: "#7E8392",
+    },
+  
+    red: "#DA181A",
+    green: "#00B969",
+  };
+  
